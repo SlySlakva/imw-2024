cebed3c271830ab1e37f2cff85e2c220fac80242
SlySlakva <93298678+SlySlakva@users.noreply.github.com>
2024-01-26 00:13:59 -0500

    E3 Assignment plus updated index

---
+++ projects/storybook 2/script.js
@@ -0,0 +1,26 @@
+//Click Function (applied to the box)
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('myBox').addEventListener('click', function() {
+        window.location.href = 'decision.html';
+    });
+});
+
+//Timer Function
+document.getElementById("four").addEventListener("animationend", startTimer); //Starts after the end of the fourth introductory message animation.
+
+function startTimer() {
+  var timeLeft = 180; // 3 minutes in seconds
+  var timerDisplay = document.getElementById("timer");
+
+  var countdown = setInterval(function() {
+    var minutes = Math.floor(timeLeft / 60);
+    var seconds = timeLeft % 60;
+
+    timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+    if (--timeLeft < 0) {
+      clearInterval(countdown);
+    }
+  }, 1000);
+}
+
