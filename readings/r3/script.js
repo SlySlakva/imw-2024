29ed9830de16dc54fc6061015e83cc8b5bec798c
SlySlakva <93298678+SlySlakva@users.noreply.github.com>
2024-03-19 21:14:04 -0400

    r3 final update woop woop

--- readings/r3/script.js
+++ readings/r3/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // Make shapes draggable
     $(".shape").draggable();
 
-    $("#image").draggable();
+    $(".image").draggable();
 
     // Make each word draggable
     $("#paragraph p").html(function (_, html) {
@@ -10,4 +10,16 @@ $(document).ready(function () {
     });
 
     $(".draggable-word").draggable();
+
+    $('.circle').click(function () {
+        $(this).toggleClass('clicked');
+    });
+
+    $('.square').click(function () {
+        $(this).toggleClass('clicked');
+    });
+
+    $('.triangle').click(function () {
+        $(this).toggleClass('clicked');
+    });
 });
